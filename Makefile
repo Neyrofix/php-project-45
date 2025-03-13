475a1077bde9672f990894c7c5ad743d108e579a
dsedra <neyrofix@gmail.com>
2025-03-13 14:53:53 +0500

    codeSniffer, codeClimate

--- Makefile
+++ Makefile
@@ -12,3 +12,6 @@ autoload:
 
 test-coverage:
 	XDEBUG_MODE=coverage composer exec --verbose phpunit tests -- --coverage-clover build/logs/clover.xml
+
+lint:
+	composer exec --verbose phpcs -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -1,11 +1,17 @@
 <?php
 
+/* Работа с командной строкой */
+
+
+
 namespace BrainGames\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-function greating(){
+/* Описание функции */
+function greating()
+{
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
